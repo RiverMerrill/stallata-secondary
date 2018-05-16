791f725cdc6064379b2f9ef5003505671d8bf5f6
River Merrill <rivermerrill.me@gmail.com>
2018-05-16 15:10:22 -0600

    working with front-end

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').Server(app);
-const gpio = require('onoff').Gpio;
-const socket = require('socket.io-client')('http://10.0.0.62:1337');
-const sensor = new gpio(2, 'in', 'both');
+//const gpio = require('onoff').Gpio;
+const socket = require('socket.io')(server);
+//const sensor = new gpio(2, 'in', 'both');
 
 sensor.watch(function(err, val){
 	if(val == 0){
-		socket.emit('update', {secondary: true});
+		socket.emit('update', {occupied: true});
 	} else{
-		socket.emit('update', {secondary: false});
+		socket.emit('update', {occupied: false});
 	}
 })	
 server.listen(1337, _ => console.log('listening'));
